3bb2805101631210f9144164e4e2a307f95f06c9
David-Halsey <dartndav@aol.com>
2019-08-15 11:00:24 -0500

    User-create folder generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,8 @@ const routes: Routes = [
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: 'users', loadChildren: './users/users.module#UsersPageModule' },
   { path: 'user', loadChildren: './user/user.module#UserPageModule' },
-  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' }
+  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' },
+  { path: 'user-create', loadChildren: './user-create/user-create.module#UserCreatePageModule' }
 ];
 
 @NgModule({
